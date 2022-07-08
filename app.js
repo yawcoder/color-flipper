6b230bd51f9c79ba54eec7f785e6a7e89c6661c0
nanayawcodes <103840077+nanayawcodes@users.noreply.github.com>
2022-07-08 16:45:30 +0000

    done

---
+++ app.js
@@ -0,0 +1,68 @@
+const bkcolor = document.querySelector('#bk');
+const btn = document.querySelector('.btn');
+let colorName = document.querySelector('.color-name');
+
+
+
+btn.addEventListener('click', () => {
+        let randomNumber = randomRange(1, 10);
+        //console.log(randomNumber);
+        //console.log(colorName.innerText);
+        if(randomNumber == 1){
+            bkcolor.removeAttribute('class');
+            bkcolor.classList.add('red');
+            colorName.innerText = '';
+            colorName.innerText += 'Red';
+        }else if(randomNumber == 2){
+            bkcolor.removeAttribute('class');
+            bkcolor.classList.add('blue');
+            colorName.innerText = '';
+            colorName.innerText += 'Blue';
+        }else if(randomNumber == 3){
+            bkcolor.removeAttribute('class');
+            bkcolor.classList.add('green');
+            colorName.innerText = '';
+            colorName.innerText += 'Green';
+        }else if(randomNumber == 4){
+            bkcolor.removeAttribute('class');
+            bkcolor.classList.add('yellow');
+            colorName.innerText = '';
+            colorName.innerText += 'Yellow';
+        }else if(randomNumber == 5){
+            bkcolor.removeAttribute('class');
+            bkcolor.classList.add('purple');
+            colorName.innerText = '';
+            colorName.innerText += 'Purple';
+        }else if(randomNumber == 6){
+            bkcolor.removeAttribute('class');
+            bkcolor.classList.add('aqua');
+            colorName.innerText = '';
+            colorName.innerText += 'Aqua';
+        }else if(randomNumber == 7){
+            bkcolor.removeAttribute('class');
+            bkcolor.classList.add('grey');
+            colorName.innerText = '';
+            colorName.innerText += 'Grey';
+        }else if(randomNumber == 8){
+            bkcolor.removeAttribute('class');
+            bkcolor.classList.add('brown');
+            colorName.innerText = '';
+            colorName.innerText += 'Brown';
+        }else if(randomNumber == 9){
+            bkcolor.removeAttribute('class');
+            bkcolor.classList.add('crimson');
+            colorName.innerText = '';
+            colorName.innerText += 'Crimson';
+        }else{
+            bkcolor.removeAttribute('class');
+            bkcolor.classList.add('violet');
+            colorName.innerText = '';
+            colorName.innerText += 'Violet';
+        }
+    });
+
+    //Function for generating random number
+    function randomRange(myMin, myMax) {
+        const num = Math.floor(Math.random() * (myMax - myMin +1)) + myMin
+        return num;
+      }
